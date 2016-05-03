2204bdf0547d0edd8cf0a0d6c6a0ab229d9de4fb
chenkangc <1203913718@qq.com>
2016-05-03 14:26:55 +0800

    1

--- validate.js
+++ validate.js
@@ -5,9 +5,9 @@
         appId: 'wxbd45231d4e653627',//fdb519c086347af5d3b641239d704252 
         //access_token:PaaVx8T9pMP718R4htJmsMlEGA9Yvv9iJowcbSZmk907l7HEbuk47JPS8oY8qc9mkx2IgU7Mb1t211rybMz_gQuRc6EQNeGR_s3iM0e7G3O3IljejnYBVoidLhCHU5yuELUbAFANUD
         //ticket:kgt8ON7yVITDhtdwci0qeaC-i0Ht508JTTmVYTF3yF43ZkvreBAU4-m39Byf3TxGAUEuvxiMP0ejzvzyQ-R-bw
-        timestamp: 1,
-        nonceStr: '',
-        signture: '',
+        timestamp: 1462256702,
+        nonceStr: 'kylhuzwop68o6ss',
+        signture: 'd005e3b393cb02d3f15732b8b59eb43682fd7d01',
         jsApiList: [
             'scanQRCode'
         ]
